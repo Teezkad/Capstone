695bdaacb6a690ace8c6e229e5e435b0ed4dc960
EricNelson12 <ericjnelson012@gmail.com>
2017-11-14 01:10:36 -0800

    Changed design to reflect the new design that we accepted. Features a side drawer menu and 3 tabs

--- Nativescript/app/app.routing.ts
+++ Nativescript/app/app.routing.ts
@@ -1,16 +1,13 @@
-import { setupPage } from "./pages/setup/setup.component";
-import { profPage} from "./pages/professionals/prof.component";
-import {patientPage} from "./pages/patient/patient.component";
+import {tabsComponent} from "./tabs/tabs.component";
+import {SideComponent} from "./sidedrawer/side.component";
 
 
 export const routes = [
-  { path: "", component: setupPage },
-  {path: "prof",component: profPage},
-  {path: "patient",component: patientPage}
+  { path: "", component: tabsComponent },
+  
 ];
 
 export const navigatableComponents = [
-  setupPage,
-  profPage,
-  patientPage
+  tabsComponent,
+  SideComponent
 ];
---
+++ Nativescript/app/tabs/tabs.component.ts
@@ -0,0 +1,13 @@
+import {Component } from "@angular/core";
+import { Page } from "ui/page"; // to hide action bar
+
+
+
+@Component({
+  selector: "tabs",
+  templateUrl: "./tabs/tabs.component.html",
+  
+  
+})
+export class tabsComponent {
+}
