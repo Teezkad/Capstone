03a3d246517ff623fe95bdf48b1d7842dceee6cf
Tunde Kadiri <tunde.kadiri96@gmail.com>
2017-11-29 23:47:03 -0800

    fixed

--- Nativescript/app/app.routing.ts
+++ Nativescript/app/app.routing.ts
@@ -1,10 +1,6 @@
 import {tabsComponent} from "./tabs/tabs.component";
 import {SideComponent} from "./sidedrawer/side.component";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> tundefork/master
 export const routes = [
   { path: "", component: tabsComponent },
   
