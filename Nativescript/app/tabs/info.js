cb0523feb20c7a1cdaa13aa37bacdb73ddf852c9
Tunde Kadiri <tunde.kadiri96@gmail.com>
2017-12-01 13:42:12 -0800

    new changes

---
+++ Nativescript/app/tabs/info.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Info = (function () {
+    function Info() {
+    }
+    return Info;
+}());
+exports.Info = Info;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5mby5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluZm8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUFBO0lBSUEsQ0FBQztJQUFELFdBQUM7QUFBRCxDQUFDLEFBSkQsSUFJQztBQUpZLG9CQUFJIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIEluZm97XHJcbiAgICBpZDogbnVtYmVyO1xyXG4gICAgcXVlc3Rpb246IHN0cmluZztcclxuICAgIGRpc2N1c2lvbjogc3RyaW5nO1xyXG59Il19
--- Nativescript/app/tabs/tabs.component.ts
+++ Nativescript/app/tabs/tabs.component.ts
@@ -4,6 +4,7 @@ import * as listViewModule from "tns-core-modules/ui/list-view";
 import * as ImageModule from "tns-core-modules/ui/image";
 import { Image } from "tns-core-modules/ui/image";
 import { OnInit } from '@angular/core';
+import { Info} from './info';
 
 
 
@@ -24,35 +25,47 @@ export class news{
   styleUrls: ['./tabs/tabs.component.css']
 })
 export class tabsComponent implements OnInit{
-  info: news[];
+  News: news[];
   constructor(){
-    this.info= [
+    this.News= [
       {id: 0,
       Topic: 'Random News about Nothing',
       author: 'James Lock', 
-      pic: "~/LilBill.png", 
+      pic: "https://www.w3schools.com" +"/images/w3schools_green.jpg", 
       prev: "This is the preview to nothing ...",
       body: "This si the preview to nothing and i have nothing else to say" },
       {id: 1,
-      Topic: 'Random News about Sopmething',
-      author: 'Yank Mike Rotch', 
-      pic: "~/LilBill.png", 
-      prev: "This is the preview to Something ...",
+      Topic: 'Who is Dr. Blankface',
+      author: 'Dr. Real Face', 
+      pic: "https://www.w3schools.com" +"/w3css/img_avatar3.png", 
+      prev: "This is the preview to who Dr. no face is dont read into it too much okay ...",
       body: "This si the preview to nothing and i have nothing else to say" },
       {id: 2,
       Topic: 'Random News about Everything',
       author: 'Yank Mike Rotch', 
-      pic: "~/LilBill.png", 
+      pic: "https://www.w3schools.com"+"/images/w3schools_green.jpg", 
       prev: "This is the preview to everything ...",
       body: "This si the preview to nothing and i have nothing else to say" },
+      {id: 3,
+        Topic: 'THE END IS NIGH!!!',
+        author: 'Reverend Donations', 
+        pic: "https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg", 
+        prev: "This is the to everything as we know it",
+        body: "This si the preview to nothing and i have nothing else to say" },
+        {id: 4,
+          Topic: 'Sayians Hate Him!!!',
+          author: 'Gangstalicious', 
+          pic: "~/images/LilBill.png", 
+          prev: "Learn how this teenager found out the secret to unlock sayian..",
+          body: "This si the preview to nothing and i have nothing else to say" }
     ]
   }
-  selectedInfo: news;
+  selectedNews: news;
     ngOnInit() {
     }
   
     onSelect(item: news): void {
-      this.selectedInfo = item;
+      this.selectedNews = item;
     }
   }
 
